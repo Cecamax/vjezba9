334ddc136ffeaf2ddab278ff1a44d5a4616a10e7
Cecamax <57287400+Cecamax@users.noreply.github.com>
2020-01-25 15:43:03 +0100

    Add files via upload

---
+++ vjezba9.php
@@ -0,0 +1,17 @@
+<?php
+
+$opis_dana = "divan";
+echo "Danas je $opis_dana dan.";
+
+echo "<br>******<br>";
+
+$opis_dana = "ruzan";
+echo "Danas je $opis_dana dan.";
+
+echo "<br>******<br>";
+
+$opis_dana = 8;
+echo "Danas je $opis_dana dan.";
+
+
+?>
